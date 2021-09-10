103531ba367d8325c7218041d7dc3afb8822899f
Jenny Lin <misslinyang91@gmail.com>
2021-09-10 11:58:57 +1000

    created tasks component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 //import React from "react";
-
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "doctors appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "school meeting",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "shopping",
+      day: "Feb 7th at 4:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
