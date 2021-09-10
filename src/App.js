65e5d94dd5f5f23b3836116053cf529d915eb71d
Jenny Lin <misslinyang91@gmail.com>
2021-09-10 16:07:34 +1000

    added task form and submit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +27,13 @@ function App() {
     },
   ]);
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -42,6 +51,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
@@ -49,7 +59,7 @@ function App() {
       )}
     </div>
   );
-}
+};
 export default App;
 // class App extends React.Component {
 //   render() {
