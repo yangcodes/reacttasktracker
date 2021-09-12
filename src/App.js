397a01a03853d5844b3a61a67208f18422e91f4f
Jenny Lin <misslinyang91@gmail.com>
2021-09-12 16:28:01 +1000

    toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -23,6 +23,13 @@ const App = () => {
     return data;
   };
 
+  //fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
   //add task
   const addTask = async (task) => {
     const res = await fetch("http://localhost:5000/tasks", {
@@ -50,10 +57,22 @@ const App = () => {
   };
 
   //toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
